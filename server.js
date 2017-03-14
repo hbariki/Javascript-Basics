41028c0af0014d58b21ef5d1d4dc4bf0e9ba88d2
Harshini <harshinibariki@gmail.com>
2017-03-14 08:43:23 -0700

    feat:Add expression prints 42 that uses 3different arithmetic operators

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by nmekala on 3/14/17.
+ */
+
+// Write an expression that uses at least 3 different arithmetic operators.
+
+   //  The expression should equal 42.
+
+// This expression equals to 42
+console.log(10*10/2-8);
